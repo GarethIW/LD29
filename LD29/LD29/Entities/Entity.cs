a7327ab2060033407a09da8b5af454100c9fcc38
Gareth Williams <gareth@garethw.net>
2014-04-25 20:10:33 +0100

    First Commit

---
+++ LD29/LD29/Entities/Entity.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameStateManagement;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TiledLib;
+
+namespace LD29.Entities
+{
+    class Entity
+    {
+        public Vector2 Position;
+        public Vector2 Speed;
+        public float Rotation;
+        public Rectangle HitBox;
+        public List<Vector2> HitPolyPoints;
+        public Texture2D SpriteSheet;
+        public bool Active;
+
+        private List<Vector2> _normalPolyPoints; 
+        private Vector2 _hitboxOffset;
+
+        public Entity(Texture2D spritesheet, Rectangle hitbox, List<Vector2> hitPolyPoints, Vector2 hitboxoffset)
+        {
+            HitBox = hitbox;
+            SpriteSheet = spritesheet;
+            Active = false;
+
+            _hitboxOffset = hitboxoffset;
+
+            if (hitPolyPoints != null)
+                _normalPolyPoints = hitPolyPoints;
+            else
+                _normalPolyPoints = new List<Vector2>()
+                {
+                    new Vector2(-(HitBox.Width / 2), -(HitBox.Height / 2)),
+                    new Vector2((HitBox.Width / 2), -(HitBox.Height / 2)),
+                    new Vector2((HitBox.Width / 2), (HitBox.Height / 2)),
+                    new Vector2(-(HitBox.Width / 2), (HitBox.Height / 2)),
+                };
+
+            HitPolyPoints = new List<Vector2>();
+            foreach(var pp in _normalPolyPoints) HitPolyPoints.Add(pp);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            if (!Active) return;
+
+            Position += Speed;
+
+            HitBox.Location = new Point((int)Position.X,(int)Position.Y) + new Point(-(HitBox.Width/2),-(HitBox.Height/2)) + new Point((int)_hitboxOffset.X, (int)_hitboxOffset.Y);
+
+            for (int index = 0; index < _normalPolyPoints.Count; index++)
+            {
+                HitPolyPoints[index] = _normalPolyPoints[index];
+                HitPolyPoints[index] = Vector2.Transform(_normalPolyPoints[index], Matrix.CreateRotationZ(Rotation));
+                HitPolyPoints[index] = HitPolyPoints[index] + _hitboxOffset + Position;
+            }
+        }
+        public virtual void Update(GameTime gameTime, Map gameMap)
+        {
+            Update(gameTime);
+        }
+
+        public virtual void OnBoxCollision(Entity collided, Rectangle intersect)
+        {}
+        public virtual void OnPolyCollision(Entity collided)
+        { }
+
+        public virtual void HandleInput(InputState input)
+        {
+            
+        }
+
+        public virtual void Draw(SpriteBatch sb)
+        {
+            if (!Active) return;
+        }
+
+        // Reset values to defaults when an entity is spawned from a pool
+        public virtual void Reset()
+        {
+            
+        }
+    }
+}
---
+++ LD29/LD29/Screens/GameplayScreen.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameStateManagement;
+using LD29.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using TiledLib;
+using TimersAndTweens;
+
+namespace LD29.Screens
+{
+    class GameplayScreen : GameScreen
+    {
+        private Camera camera;
+        private Map map;
+
+        private ParticleController particleController = new ParticleController();
+
+        public GameplayScreen()
+        {
+            TransitionOnTime = TimeSpan.FromSeconds(0.5);
+            TransitionOffTime = TimeSpan.FromSeconds(0.5);
+        }
+
+        public override void LoadContent()
+        {
+            ContentManager content = ScreenManager.Game.Content;
+
+            map = content.Load<Map>("map");
+
+            camera = new Camera(ScreenManager.Game.RenderWidth, ScreenManager.Game.RenderHeight, map);
+
+            particleController.LoadContent(content);
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            camera.Update(gameTime);
+           
+            particleController.Update(gameTime, map);
+
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Vector2 center = new Vector2(ScreenManager.Game.RenderWidth, ScreenManager.Game.RenderHeight) / 2f;
+            SpriteBatch sb = ScreenManager.SpriteBatch;
+
+            sb.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, camera.CameraMatrix);
+            map.DrawLayer(sb, "fg", camera);
+            sb.End();
+
+            particleController.Draw(sb, camera, 1);
+
+            sb.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null);
+            sb.End();
+
+            ScreenManager.FadeBackBufferToBlack(1f - TransitionAlpha);
+
+            base.Draw(gameTime);
+
+        }
+    }
+}
